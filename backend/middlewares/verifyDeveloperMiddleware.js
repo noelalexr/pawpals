5682bbdf4149b4b52951af1167754f637d8cd50e
Noel John Hementera <dev.noeljohn@gmail.com>
2025-06-27 22:46:33 +0800

    feat: add index.js, update developer controller, auth, and verifyDeveloper middlewares

--- backend/middlewares/verifyDeveloperMiddleware.js
+++ backend/middlewares/verifyDeveloperMiddleware.js
@@ -1,8 +1,35 @@
-const verifyDeveloper = (req, res, next) => {
-    if(req.userType !== "developer") {
-        return res.status(403).json({ message: "Access denied. Developer role required." })
-    }
-    next(); //moves on to the next middleware or route
-}
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+import Developer from "../models/developerSchema.js";
 
-export default verifyDeveloper;
+dotenv.config();
+
+const verifyDeveloper = async (req, res, next) => {
+    try{
+        if(!req.cookies.token) {
+            return res.status(401).json({ error: "Unauthorized Access" });
+        }
+
+        const token = req.cookies.token;
+
+        if(!token) {
+            return res.status(401).json({ error: "Unauthorized Access" });
+        }
+
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+        const developer = await Developer.findById(decoded.id);
+
+        if(!developer || developer.role !== "developer"){
+            return res.status(403).json({ error: "Access denied: Developers only" });
+        }
+
+        req.developer = developer; //gets the devs info
+
+        next();
+    }catch(err){
+        res.status(500).json({ error: err.message });
+    }
+};
+
+export { verifyDeveloper };
