2bc90224c10d2b0ceab2a55702055b42b4e43ab8
Roman Gavino <romgavino@gmail.com>
2025-06-29 22:31:10 +0800

    fix: switched enum from breed to species

--- backend/models/petSchema.js
+++ backend/models/petSchema.js
@@ -4,8 +4,8 @@ const petSchema = new mongoose.Schema({
         name: { type: String, required: true},
         age: { type: Number, required: true },
         arrivalDate: { type: Date, required: true },
-        breed: { type: String, required: true, enum: ["dog", "cat", "bird"] },
-        species: { type: String, required: true },
+        breed: { type: String, required: true },
+        species: { type: String, required: true, enum: ["dog", "cat", "bird"] },
         gender: {type: String, required: true, enum: ["male", "female", "undetermined"]},
         description: { type: String, required: true },
         images: [
