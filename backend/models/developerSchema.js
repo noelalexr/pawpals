7099c1a1fea6d81ec02dc3d869c2a70b86d8e067
Noel John Hementera <dev.noeljohn@gmail.com>
2025-06-29 08:57:21 +0800

    fix: 3 schema models. Removed createdAt then replaced it with timestamps

--- backend/models/developerSchema.js
+++ backend/models/developerSchema.js
@@ -1,12 +1,13 @@
 import mongoose from "mongoose";
 
 const developerSchema = new mongoose.Schema({
-    name: { type: String, required: true },
-    email: { type: String, required: true, unique: true},
-    password: { type: String, required: true},
-    role: { type: String, default: "developer" },
-    createdAt: { type: Date, default: Date.now }
-});
+        name: { type: String, required: true },
+        email: { type: String, required: true, unique: true},
+        password: { type: String, required: true},
+        role: { type: String, default: "developer" },
+    },
+    { timestamps: true }
+);
 
 const developerModel = mongoose.model("Developer", developerSchema);
 
