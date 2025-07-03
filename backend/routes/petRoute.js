4c572c3cb4927b28484e5f51bc5ce77c2cdfcec3
Alexander Noel <noelalexr@gmail.com>
2025-07-03 15:21:39 +0800

    feat: update petRoute

--- backend/routes/petRoute.js
+++ backend/routes/petRoute.js
@@ -1,6 +1,6 @@
 import express from "express";
-import {createPet, listPet, listMyPets, readPet, readMyPet, patchPet, deletePet, deletePetPhoto} from "../controllers/petController.js"
-import {auth} from "../middlewares/authMiddleware.js"
+import { createPet, listPet, listMyPets, readPet, readMyPet, patchPet, deletePet, deletePetPhoto } from "../controllers/petController.js"
+import { auth } from "../middlewares/authMiddleware.js"
 import upload from "../middlewares/upload.js";
 
 const router = express.Router()
@@ -9,7 +9,7 @@ router.post("/", auth, upload.array("images", 3), createPet);
 router.get("/", listPet);
 router.get("/:id", readPet);
 router.get("/mine", auth, listMyPets);
-router.get("/mine/:id", auth, readMyPet);
+router.get("/mine/:id", auth, readPet);
 router.patch("/mine/:id", auth, upload.array("images", 3), patchPet);
 router.delete("/mine/:id", auth, deletePet);
 router.delete("/mine/:id/photos/:publicId", auth, deletePetPhoto);
