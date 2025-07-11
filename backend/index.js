7b205662e0574d1003deb267e79384b34ab869ac
Alexander Noel <noelalexr@gmail.com>
2025-07-11 21:45:29 +0800

    feat: revert changes in cors of index.js

--- backend/index.js
+++ backend/index.js
@@ -28,13 +28,7 @@ const allowedOrigins = [
 //middleware
 app.use(helmet());
 app.use(cors({
-    origin: function (origin, callback) {
-        if (!origin || allowedOrigins.includes(origin)) {
-            callback(null, true);
-        } else {
-            callback(new Error("Not allowed by CORS"));
-        }
-    },
+    origin: allowedOrigins,
     credentials: true
 }));
 app.use(express.json());
