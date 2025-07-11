75ce9d39f240250d9e3e3cf2b567e93c292d4072
Alexander Noel <noelalexr@gmail.com>
2025-07-11 21:41:49 +0800

    feat: update cors to support multiple path for ceredentials

--- backend/index.js
+++ backend/index.js
@@ -28,7 +28,13 @@ const allowedOrigins = [
 //middleware
 app.use(helmet());
 app.use(cors({
-    origin: allowedOrigins,
+    origin: function (origin, callback) {
+        if (!origin || allowedOrigins.includes(origin)) {
+            callback(null, true);
+        } else {
+            callback(new Error("Not allowed by CORS"));
+        }
+    },
     credentials: true
 }));
 app.use(express.json());
